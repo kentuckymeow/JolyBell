7a2620df1f126ef7bf82e41ccf425ad7e125991d
ArseniKhatsuk <khatsuk007@gmail.com>
2022-05-25 00:25:22 +0300

    Initial Commit

---
+++ JolyBell/JolyBellApp.swift
@@ -0,0 +1,17 @@
+//
+//  JolyBellApp.swift
+//  JolyBell
+//
+//  Created by Arseni Khatsuk on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct JolyBellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
